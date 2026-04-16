bd1d75771c40cee1e327a53fca3689c00ff7c5a1
satan <leejs8266@gmail.com>
2026-04-16 23:55:50 +0900

    feat: apply sqlalchemy mapped syntax

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, Boolean, String
+from sqlalchemy import Integer, Boolean, String
+from sqlalchemy.orm import Mapped, mapped_column
 from app.core.database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    idx = Column(Integer, primary_key=True, index=True)
-    email = Column(String(255), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    is_active = Column(Boolean, default=True)
+    idx: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, index=True, nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
