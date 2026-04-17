51669dbe8190127061a6bfcac56f28cc6ae07ba2
satan <leejs8266@gmail.com>
2026-04-18 02:13:12 +0900

    feat: remove chat service

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base
-from app.models import User
-from app.controllers import webhook_controller, user_controller, chat_controller
+from app.models import User, Incident
+from app.controllers import webhook_controller, user_controller
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,4 +10,3 @@ app = FastAPI()
 # 컨트롤러의 라우터를 등록
 app.include_router(webhook_controller.router)
 app.include_router(user_controller.router)
-app.include_router(chat_controller.router)
--- app/schemas/chat_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class ChatRequest(BaseModel):
-    message: str
-
-class ChatResponse(BaseModel):
-    reply: str
